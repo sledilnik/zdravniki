a7fed8b41b225badd4dbdaa4b055e7ced51d015a
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-10-23 08:34:54 +0200

    FEAT(constants): adds doctor's field translation

---
+++ src/constants/doctors.js
@@ -0,0 +1,13 @@
+export const Accepts = {
+  NE: 'ne sprejema',
+  DA: 'sprejema',
+};
+
+export const Activities = {
+  'SPLOŠNA DEJ.-OTROŠKI IN ŠOLSKI DISPANZER': 'OTROŠKI IN ŠOLSKI DISPANZER',
+  'SPLOŠNA DEJAVNOST - SPLOŠNA AMBULANTA': 'SPLOŠNA AMBULANTA',
+};
+
+export const Provider = {
+  ZD: 'ZDRAVSTVENI DOM',
+};
