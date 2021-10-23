8b3c1ab8306bca316043cb6c583e0c3c08f98d3c
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-10-23 08:30:45 +0200

    FEAT(hooks): add fetch and parse csv files

---
+++ src/hooks/index.js
@@ -0,0 +1,1 @@
+export { useFetchAndParseCsv } from './useFetchAndParseCsv';
---
+++ src/hooks/useFetchAndParseCsv.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import { readRemoteFile } from 'react-papaparse';
+
+const useFetchAndParseCsv = url => {
+  const [parsed, setParsed] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchCsv = async () => {
+      setIsFetching(true);
+      try {
+        const response = await fetch(url);
+        const blob = await response.blob();
+        readRemoteFile(blob, {
+          complete: results => {
+            const { data, errors } = results;
+            if (errors.length > 0) {
+              console.log(errors);
+              throw new Error('Somenthing went wrong during parsing csv file!');
+            }
+
+            if (!data) {
+              throw new Error('No data in csv file!');
+            }
+
+            data && setParsed(data);
+          },
+        });
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsFetching(false);
+      }
+    };
+
+    fetchCsv();
+  }, []);
+  return { parsed, isFetching, error };
+};
+
+export default useFetchAndParseCsv;
