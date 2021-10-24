b5158cb6c37efdff7ec7dbb21b51f81a84a75cbc
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-10-24 17:26:45 +0200

    FEAT(Shared)(Grid): add Doctors and Loader

--- src/components/Shared/Grid.js
+++ src/components/Shared/Grid.js
@@ -1,6 +1,22 @@
 import { styled } from '@mui/material/styles';
+
 export const Cards = styled('div')(({ theme }) => ({
   display: 'grid',
   gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
   gridGap: '1rem',
+}));
+
+export const Doctors = styled('div')(({ theme }) => ({
+  display: 'grid',
+  gridTemplateRows: '0.1fr 0.8fr 0.1fr',
+  gridGap: '1rem',
+  alignContent: 'stretch',
+  alignItems: 'start',
+}));
+
+export const Loader = styled('div')(({ theme }) => ({
+  display: 'grid',
+  gridAutoRows: '1fr',
+  alignItems: 'center',
+  minHeight: '50vh',
 }));
