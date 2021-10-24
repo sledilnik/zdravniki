1183c1504db58c939b9f8b3434182d15beeba5bf
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-10-24 05:11:33 +0200

    FEAT(components): add ChooseDoctorType

---
+++ src/components/ChooseDoctorType.js
@@ -0,0 +1,31 @@
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import { doctorsByTypeContext } from '../context';
+
+export default function ChooseDoctorType() {
+  const { doctorType, setDoctorType } = doctorsByTypeContext.useDoctorsByType();
+
+  const onChangeHandler = event => {
+    setDoctorType(event.target.value);
+  };
+
+  return (
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Tip zdravnika</FormLabel>
+      <RadioGroup
+        row
+        aria-label="tip"
+        name="row-radio-buttons-group"
+        value={doctorType}
+        onChange={onChangeHandler}
+      >
+        <FormControlLabel value="doctors" control={<Radio />} label="Splošni" />
+        <FormControlLabel value="gyno" control={<Radio />} label="Ginekolog" />
+        <FormControlLabel value="dentists" control={<Radio />} label="Zobozdravnik" />
+      </RadioGroup>
+    </FormControl>
+  );
+}
