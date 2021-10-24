dc819fb4b52cded12dcec84547879f6f1b6f8e30
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-10-24 12:08:55 +0200

    FEAT(constants)(doctors): add translations for ...
    ... bool and add dentists/gyno activities translations

--- src/constants/doctors.js
+++ src/constants/doctors.js
@@ -1,11 +1,19 @@
-export const Accepts = {
+export const AcceptsText = {
   NE: 'ne sprejema',
   DA: 'sprejema',
+};
+
+export const AcceptsBool = {
+  NE: false,
+  DA: true,
 };
 
 export const Activities = {
   'SPLOŠNA DEJ.-OTROŠKI IN ŠOLSKI DISPANZER': 'OTROŠKI IN ŠOLSKI DISPANZER',
   'SPLOŠNA DEJAVNOST - SPLOŠNA AMBULANTA': 'SPLOŠNA AMBULANTA',
+  'ZOBOZDR. DEJAVNOST-ZDRAVLJENJE ODRASLIH': 'ZDRAVLJENJE ODRASLIH',
+  'ZOBOZDR. DEJAVNOST-ZDRAVLJENJE MLADINE': 'ZDRAVLJENJE MLADINE',
+  'SPLOŠNA DEJAVNOST - DISPANZER ZA ŽENSKE': 'DISPANZER ZA ŽENSKE',
 };
 
 export const Provider = {
