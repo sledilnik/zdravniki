0d1274fafcb5f73ad30866127ed4eede57221c4b
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-10-24 12:04:05 +0200

    FEAT(components)(Shared): add ExpandMore

---
+++ src/components/Shared/ExpandMore.js
@@ -0,0 +1,23 @@
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const ExpandMoreButton = styled(({ expand, ...other }) => {
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+const ExpandMore = ({ expand, ...other }) => {
+  return (
+    <ExpandMoreButton expand={expand} {...other}>
+      <ExpandMoreIcon />
+    </ExpandMoreButton>
+  );
+};
+
+export default ExpandMore;
