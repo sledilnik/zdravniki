1147556dd5919ca337207aa6bef57a3fa3369827
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-10-24 17:36:27 +0200

    REFACTOR(components): move Loader to Shared folder

--- src/components/Shared/index.js
+++ src/components/Shared/index.js
@@ -2,3 +2,4 @@ export * as Chip from './Chip';
 export { default as ExpandMore } from './ExpandMore';
 export * as Grid from './Grid';
 export * as Pagination from './Pagination';
+export { default as Loader } from './Loader';
