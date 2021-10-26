82537b02a169e8cfcbfe20ba2f4d307733417212
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-10-26 17:47:47 +0200

    REFACTOR(components)(Shared)(Leaflet): -forwardRef

--- src/components/Shared/Leaflet/index.js
+++ src/components/Shared/Leaflet/index.js
@@ -1,18 +1,15 @@
-import { forwardRef } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 
 export * as Markers from './Markers';
 
-const Leaflet = forwardRef(({ children, height, ...other }, ref) => {
-  return (
-    <MapContainer ref={ref} style={{ height }} {...other}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {children}
-    </MapContainer>
-  );
-});
+const Leaflet = ({ children, height, ...other }) => (
+  <MapContainer style={{ height }} {...other}>
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    {children}
+  </MapContainer>
+);
 
 export default Leaflet;
