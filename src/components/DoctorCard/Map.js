2bbaa6f01dd30449bcd3dd55a2ab323781754414
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-10-26 17:47:48 +0200

    FEAT(components)(DoctorsCard): overide props..
    ... some props are hardcoded. Now you can overide them: zoom,
    dragging, zoomControl and schrollWheelZoom

--- src/components/DoctorCard/Map.js
+++ src/components/DoctorCard/Map.js
@@ -1,18 +1,23 @@
+import { geoLocation } from '../../constants';
 import Leaflet, { Markers } from '../Shared/Leaflet';
 
 function withLeaflet(Component) {
-  const DoctorMap = ({ doctor, height, ...other }) => {
+  const defaultGeoLocation = { lat: geoLocation.SL_CENTER[0], lon: geoLocation.SL_CENTER[1] };
+
+  const DoctorMap = ({ doctor, height = '200px', ...other }) => {
     const {
       geoLocation: { lat, lon },
-    } = doctor ?? { geoLocation: { lat: 45, lon: 14 } };
+    } = doctor ?? { geoLocation: defaultGeoLocation };
     const position = [lat, lon];
+
     const injectedProps = {
-      ...other,
       center: position,
       height,
       zoom: 13,
       dragging: false,
       zoomControl: false,
+      scrollWheelZoom: false,
+      ...other,
     };
     return (
       <Component {...injectedProps}>
