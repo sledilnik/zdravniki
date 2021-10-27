c6eb0b95dba95b7a2dee51e256298b15324572ce
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-10-27 17:45:58 +0200

    CSS(components)(Shared)(Grid)(App): no gap

--- src/components/Shared/Grid.js
+++ src/components/Shared/Grid.js
@@ -7,6 +7,7 @@ const BaseGrid = styled('div')(({ theme }) => ({
 
 export const App = styled(BaseGrid)(({ theme }) => ({
   gridTemplateRows: 'min-content 1fr min-content',
+  gridGap: 0,
 }));
 export const Cards = styled(BaseGrid)(({ theme }) => ({
   gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
