f5a7e56fa44e4e5674c46928314ac71f926c3f0d
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-10-31 06:14:57 +0100

    FEAT(Doctors): map height on very small screens

--- src/components/Doctors/index.js
+++ src/components/Doctors/index.js
@@ -1,13 +1,15 @@
 import { useEffect, useMemo, useState } from 'react';
 import { styled } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import json2mq from 'json2mq';
 
 import { filterContext } from 'context';
 import MainMap from './Map';
 import DoctorCard from 'components/DoctorCard';
 import { useLeafletContext } from 'context/leafletContext';
 import { GEO_LOCATION } from 'constants/index';
-import { Button } from '@mui/material';
+import Button from '@mui/material/Button';
 
 const StyledWrapper = styled('div')(({ theme }) => ({
   scrollBehavior: 'smooth',
@@ -20,6 +22,11 @@ const ButtonWrapper = styled('div')(({ theme }) => ({
 }));
 
 const Doctors = ({ itemsPerPage = 10 }) => {
+  // ? not sure what to do with landscape small mobile
+  const upXS = json2mq({ screen: true, minWidth: 350 });
+  const isUpXS = useMediaQuery(upXS);
+  const mapHeight = isUpXS ? '500px' : '300px';
+
   const { doctors, doctorType, accept, searchValue, ids, setIds } = filterContext.useFilter();
   const { map, setMap } = useLeafletContext();
   const [items, setItems] = useState(Array.from({ length: itemsPerPage }));
@@ -51,9 +58,14 @@ const Doctors = ({ itemsPerPage = 10 }) => {
     setIds([]);
   };
 
+  /**
+   * use mapHeight as key on MainMap to force re - render
+   * Not sure why it didn't re-render on prop change
+   */
+
   return (
     <StyledWrapper>
-      <MainMap whenCreated={setMap} doctors={doctors} />
+      <MainMap key={mapHeight} whenCreated={setMap} doctors={doctors} height={mapHeight} />
       {ids.length === 1 && (
         <ButtonWrapper>
           <Button onClick={handleShowAll}>Pokaži vse</Button>
