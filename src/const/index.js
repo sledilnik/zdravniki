1462deb5f158b92c483c1ce2a1cb32e6c2ee4781
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-10-31 11:53:25 +0100

    FEAT(constants): add device sizes, map heights

--- src/const/index.js
+++ src/const/index.js
@@ -1,3 +1,4 @@
 export * as APP_URL from './url';
 export * as DOCTORS from './doctors';
 export * as GEO_LOCATION from './geoLocation';
+export * as SIZES from './sizes';
---
+++ src/const/sizes.js
@@ -0,0 +1,14 @@
+// not MUI breakpoints
+export const DEVICES = {
+  xxs: 320,
+  xs: 481,
+  s: 641,
+  m: 961,
+  l: 1025,
+  xl: 1281,
+};
+
+export const MAP_HEIGHT = {
+  small: 280,
+  large: 500,
+};
