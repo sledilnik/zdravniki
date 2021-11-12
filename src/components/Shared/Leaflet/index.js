8d55260714c6a808867b6170e8a59405c8f5b0bd
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-11-12 15:11:18 +0100

    FEAT(components)(Shared)(Leaflet): memoize

--- src/components/Shared/Leaflet/index.js
+++ src/components/Shared/Leaflet/index.js
@@ -1,15 +1,18 @@
+import { memo } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 
 export * as Markers from './Markers';
 
-const Leaflet = ({ children, height, ...other }) => (
-  <MapContainer style={{ height }} {...other}>
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    {children}
-  </MapContainer>
-);
+const Leaflet = ({ children, height, ...other }) => {
+  return (
+    <MapContainer style={{ height }} {...other}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {children}
+    </MapContainer>
+  );
+};
 
-export default Leaflet;
+export default memo(Leaflet);
