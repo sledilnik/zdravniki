1469fdc0ba4e3205959701a7dc6e5a238fc92729
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-11-14 09:41:47 +0100

    FEAT(const): add url for sledilnik zdravniki data

--- src/const/index.js
+++ src/const/index.js
@@ -2,3 +2,4 @@ export * as APP_URL from './url';
 export * as DOCTORS from './doctors';
 export * as GEO_LOCATION from './geoLocation';
 export * as SIZES from './sizes';
+export * as CSV_URL from './csvURL';
