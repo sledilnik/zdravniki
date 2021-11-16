0357a894ceb77ad6b6ebd41ac551f56381509cdf
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-11-16 10:26:57 +0100

    FEAT(components)(Shared)(Icons): + EmojiPeopleIcon

--- src/components/Shared/Icons.js
+++ src/components/Shared/Icons.js
@@ -1,3 +1,4 @@
 export { default as MenuIcon } from '@mui/icons-material/Menu';
 export { default as RoomIcon } from '@mui/icons-material/Room';
 export { default as SearchIcon } from '@mui/icons-material/Search';
+export { default as EmojiPeopleIcon } from '@mui/icons-material/EmojiPeople';
