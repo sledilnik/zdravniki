16a3f5a5ea9022ac2f25284302190ba1c7cb5588
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-11-17 17:39:42 +0100

    FEAT(components)(Shared): + ToggleGroup

---
+++ src/components/Shared/ToggleGroup.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+
+import Paper from '@mui/material/Paper';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
+const StyledToggleButtonGroup = styled(ToggleButtonGroup)(({ theme }) => ({
+  '& .MuiToggleButtonGroup-grouped': {
+    margin: theme.spacing(0.5),
+    border: 0,
+    '&.Mui-disabled': {
+      border: 0,
+    },
+    '&:not(:first-of-type)': {
+      borderRadius: theme.shape.borderRadius,
+    },
+    '&:first-of-type': {
+      borderRadius: theme.shape.borderRadius,
+    },
+  },
+}));
+
+export default function ToggleGroup({ children, value, setValue }) {
+  const handleAlignment = (event, newAlignment) => {
+    setValue(newAlignment);
+  };
+
+  return (
+    <Paper
+      elevation={0}
+      sx={{
+        display: 'flex',
+        border: theme => `1px solid ${theme.palette.divider}`,
+        flexWrap: 'wrap',
+        width: 'max-content',
+      }}
+    >
+      <StyledToggleButtonGroup
+        size="small"
+        value={value}
+        exclusive
+        onChange={handleAlignment}
+        aria-label="text alignment"
+      >
+        {children}
+      </StyledToggleButtonGroup>
+    </Paper>
+  );
+}
