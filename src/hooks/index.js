7c02f66bcf9c5f027173c6704629c46583110ded
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-11-20 18:06:44 +0100

    FEAT(hooks): add useEventListener()

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,3 +2,4 @@ export { default as useFetchAndParseCsv } from './useFetchAndParseCsv';
 export { default as useTimeout } from './useTimeout';
 export { default as useDebounce } from './useDebounce';
 export { default as useGeoLocation } from './useGeoLocation';
+export { default as useEventListener } from './useEventListener';
---
+++ src/hooks/useEventListener.js
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react';
+
+export default function useEventListener(
+  eventType,
+  callback,
+  element = window,
+  useCapture = false,
+) {
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (element == null) return;
+    const handler = e => callbackRef.current(e);
+    element.addEventListener(eventType, handler, useCapture);
+
+    return () => element.removeEventListener(eventType, handler);
+  }, [eventType, element, useCapture]);
+}
