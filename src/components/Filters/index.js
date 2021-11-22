017937004840f737f1ee936c111ad22a6531fd2e
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-11-22 01:50:09 +0100

    REFACTOR(components)(Filter): + styled Container

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -1,22 +1,15 @@
-import Container from '@mui/material/Container';
 import ToggleAccepts from './ToggleAccepts';
 import ToggleDoctorType from './ToggleDoctorType';
 import Search from './Search';
 
+import * as Styled from './styles';
+
 export default function Filters() {
   return (
-    <Container
-      maxWidth="false"
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        flexWrap: 'wrap',
-        boxShadow: '0 3px 10px 0 rgba(0,0,0,0.06)',
-      }}
-    >
+    <Styled.Container maxWidth="false">
       <ToggleDoctorType />
       <ToggleAccepts />
       <Search />
-    </Container>
+    </Styled.Container>
   );
 }
--- src/components/Filters/styles/index.js
+++ src/components/Filters/styles/index.js
@@ -1,1 +1,17 @@
+import { styled } from '@mui/material/styles';
+
+import MuiContainer from '@mui/material/Container';
+
 export * as Search from './Search';
+export * as Icon from './Icon';
+
+export const Container = styled(MuiContainer)(({ theme }) => ({
+  // ? better to switch with Box
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  flexWrap: 'wrap',
+  boxShadow: '0 3px 10px 0 rgba(0,0,0,0.06)',
+  paddingLeft: theme.spacing(0.5),
+  paddingRight: theme.spacing(0.5),
+}));
