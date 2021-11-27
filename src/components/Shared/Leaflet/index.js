6235940899ae8c1cefba69b3b9eb980b1d813754
Štefan Baebler <stefan.baebler@gmail.com>
2021-11-27 20:21:07 +0100

    Use toner map tiles by stamen design (#10)

--- src/components/Shared/Leaflet/index.js
+++ src/components/Shared/Leaflet/index.js
@@ -7,8 +7,8 @@ const Leaflet = ({ children, height, ...other }) => {
   return (
     <MapContainer style={{ height }} {...other}>
       <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='Map tiles by <a href="https://stamen.com/">Stamen Design</a>, under <a href="https://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png"
       />
       {children}
     </MapContainer>
