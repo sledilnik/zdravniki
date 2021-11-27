24970d175ab9dc3638b0f6a2859b1bf2b69af05e
Miha Eržen <miha.erzen1@gmail.com>
2021-11-27 19:08:36 +0100

    📦 Add eslint and prettier packages

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ['airbnb', 'airbnb/hooks', 'prettier'],
+  plugins: ['react', 'prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/prefer-default-export': 'off'
+  },
+};
