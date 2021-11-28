47b6937b7358feadb93f8fb4aaae9195341aa038
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-11-28 08:06:59 +0100

    REFACTOR(App): move theme to conts folder

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from 'pages/Home';
 import About from 'pages/About';
 import Doctor from 'pages/Doctor';
 import { t } from 'i18next';
+import { THEME } from 'const';
 
 function App() {
   const lng = localStorage.getItem('i18nextLng') || 'sl';
@@ -117,31 +118,7 @@ function App() {
 }
 
 function AppWrapper() {
-  const theme = createTheme({
-    typography: {
-      fontFamily: ['IBM Plex Sans', 'Roboto', 'sans-serif'].join(','),
-    },
-    customColors: {
-      accent: '#09AFDA',
-      brand: '#95C83F',
-      danger: '#DC3435',
-      dark: '#212529',
-      lightGrey: '#CDCDCD',
-      background: '#F0F0E8',
-      text: 'rgba(0,0,0,.56)',
-      link: 'rgba(33,37,41,0.56);',
-    },
-    customOpacity: {
-      half: 0.56,
-    },
-    MD: {
-      textColor: 'rgba(0, 0, 0, 0.7)',
-      linkColor: 'rgba(0, 0, 0, 0.8)',
-      elementBoxShadow: '0 1px 3px 0 rgba(0, 0, 0, 0.15)',
-      linkBoxShadow: 'inset 0 -1px 0 white, inset 0 -4px #95C83F',
-      linkBoxShadowHover: 'inset 0 -1px 0 white, inset 0 -20px #95C83F',
-    },
-  });
+  const theme = createTheme(THEME);
   return (
     <>
       <CssBaseline />
---
+++ src/const/theme.js
@@ -0,0 +1,27 @@
+const theme = {
+  typography: {
+    fontFamily: ['IBM Plex Sans', 'Roboto', 'sans-serif'].join(','),
+  },
+  customColors: {
+    accent: '#09AFDA',
+    brand: '#95C83F',
+    danger: '#DC3435',
+    dark: '#212529',
+    lightGrey: '#CDCDCD',
+    background: '#F0F0E8',
+    text: 'rgba(0,0,0,.56)',
+    link: 'rgba(33,37,41,0.56);',
+  },
+  customOpacity: {
+    half: 0.56,
+  },
+  MD: {
+    textColor: 'rgba(0, 0, 0, 0.7)',
+    linkColor: 'rgba(0, 0, 0, 0.8)',
+    elementBoxShadow: '0 1px 3px 0 rgba(0, 0, 0, 0.15)',
+    linkBoxShadow: 'inset 0 -1px 0 white, inset 0 -4px #95C83F',
+    linkBoxShadowHover: 'inset 0 -1px 0 white, inset 0 -20px #95C83F',
+  },
+};
+
+export default theme;
