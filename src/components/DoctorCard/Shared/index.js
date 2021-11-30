e0b82b7f7b9978b53ff290771ab356ba29d79f1f
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-11-30 06:40:58 +0100

    FEAT(comp...)(DoctorCard)(Shared): + DoubleChip

--- src/components/DoctorCard/Shared/index.js
+++ src/components/DoctorCard/Shared/index.js
@@ -1,3 +1,15 @@
+import { styled } from '@mui/material/styles';
+
+import { t } from 'i18next';
+import {
+  TypeTranslate,
+  AgeGroupTranslate,
+  TypeIconTranslate,
+  AgeGroupIconTranslate,
+} from '../dicts';
+import * as Icons from 'components/Shared/Icons';
+
+import { Stack } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import * as Styled from '../styles';
 
@@ -14,3 +26,53 @@ export const ConditionalLink = ({ children, to, component = 'div' }) => {
     </Typography>
   );
 };
+
+export const DoubleChip = ({ type, ageGroup }) => {
+  const drType = TypeTranslate[type];
+  const drAgeGroup = AgeGroupTranslate?.[ageGroup] ?? 'adults';
+  const typeIcon = TypeIconTranslate[type];
+  const ageGroupIcon = AgeGroupIconTranslate?.[ageGroup] ?? 'Adults';
+  return (
+    <DCWrapper direction="row">
+      <First direction="row" component="span" spacing={1}>
+        <Icons.Icon name={typeIcon} className="icon" />
+        <span className="text">{t(`${drType}`)}</span>
+      </First>
+      <Second direction="row" component="span" spacing={1}>
+        <span className="text">{t(`${drAgeGroup}`)}</span>
+        <Icons.Icon name={ageGroupIcon} className="icon" />
+      </Second>
+    </DCWrapper>
+  );
+};
+
+export const DCWrapper = styled(Stack)(({ theme }) => {
+  return {
+    color: theme.customColors.doctor.colors.chip,
+    fontSize: '0.8125rem',
+    fontWeight: 400,
+    letterSpacing: 0,
+    borderRadius: '5px',
+    '.icon': {
+      opacity: 0.25,
+    },
+    '.text': {
+      opacity: 0.56,
+    },
+  };
+});
+
+const First = styled(Stack)(({ theme }) => {
+  return {
+    backgroundColor: '#F5F5F1',
+    padding: '6px 8px',
+    borderRadius: '5px 0 0 5px',
+  };
+});
+const Second = styled(Stack)(({ theme }) => {
+  return {
+    backgroundColor: '#EAEAE3',
+    padding: '6px 8px',
+    borderRadius: '0 5px 5px 0',
+  };
+});
