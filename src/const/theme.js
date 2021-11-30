d40e8421d6555d8301fea6bb9ecb906a7ed33da6
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-11-30 05:19:45 +0100

    FEAT(const)(theme): add doctor's colors

--- src/const/theme.js
+++ src/const/theme.js
@@ -1,16 +1,36 @@
+const colors = {
+  accent: '#09AFDA',
+  brand: '#95C83F',
+  danger: '#DC3435',
+  dark: '#212529',
+  lightGrey: '#CDCDCD',
+  background: '#F0F0E8',
+  text: 'rgba(0,0,0,.56)',
+  link: 'rgba(33,37,41,0.56);',
+  nickel: '#6C7074',
+  philippineSilver: '#B1B1B1',
+};
+
 const theme = {
   typography: {
     fontFamily: ['IBM Plex Sans', 'Roboto', 'sans-serif'].join(','),
   },
   customColors: {
-    accent: '#09AFDA',
-    brand: '#95C83F',
-    danger: '#DC3435',
-    dark: '#212529',
-    lightGrey: '#CDCDCD',
-    background: '#F0F0E8',
-    text: 'rgba(0,0,0,.56)',
-    link: 'rgba(33,37,41,0.56);',
+    ...colors,
+    doctor: {
+      colors: {
+        name: colors.dark,
+        provider: colors.dark,
+        link: colors.accent,
+        address: colors.nickel,
+        availability: colors.dark,
+        load: colors.philippineSilver,
+        type: colors.dark,
+      },
+      opacity: {
+        type: 0.56,
+      },
+    },
   },
   customOpacity: {
     half: 0.56,
