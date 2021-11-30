cc7b9266f5bbb2969da2a7fe3560e8552015cc8e
Stefan Baebler <stefan.baebler@gmail.com>
2021-11-30 09:13:42 +0100

    Trying toner-lite map style

--- src/components/Shared/Leaflet/index.js
+++ src/components/Shared/Leaflet/index.js
@@ -8,7 +8,7 @@ const Leaflet = ({ children, height, ...other }) => {
     <MapContainer style={{ height }} {...other}>
       <TileLayer
         attribution='Map tiles by <a href="https://stamen.com/">Stamen Design</a>, under <a href="https://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png"
+        url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png"
       />
       {children}
     </MapContainer>
