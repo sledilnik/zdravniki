794ef149875f1ea3e0688d1380c30fd447d300ee
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-11-30 05:21:56 +0100

    FEAT(comp...)(DoctorCard): add dicts and utils

---
+++ src/components/DoctorCard/dicts.js
@@ -0,0 +1,29 @@
+export const DoctorTypeTranslate = {
+  gp: 'zdravnik',
+  'gp-y': 'zdravnik',
+  den: 'zobozdravnik',
+  'den-s': 'zobozdravnik',
+  'den-y': 'zobozdravnik',
+  gyn: 'ginekolog',
+};
+export const TypeTranslate = {
+  gp: 'generalPractitioner',
+  den: 'dentist',
+  gyno: 'gynecologist',
+};
+
+export const AgeGroupTranslate = {
+  y: 'youth',
+  s: 'students',
+};
+
+export const TypeIconTranslate = {
+  gp: 'Family',
+  den: 'Dentists',
+  gyno: 'Gyno',
+};
+
+export const AgeGroupIconTranslate = {
+  y: 'Kids',
+  s: 'Students',
+};
