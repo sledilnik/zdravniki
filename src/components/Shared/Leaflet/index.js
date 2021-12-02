e30ab164940f43d9fdb63308bb8c50ac73954d33
bananica <bane@bananica.com>
2021-12-02 01:52:37 +0100

    Change map styling (#46)
    * Change map styling - revert to default OSM tiles, tweak the look of the tiles with CSS to put emphasis on the markers
    
    * Changed attribution link to https
    
    * Use styled components for map styling, remove opacity (performance concerns) use hue shift instead
    
    Co-authored-by: Štefan Baebler <stefan.baebler@gmail.com>

--- src/components/Shared/Leaflet/index.js
+++ src/components/Shared/Leaflet/index.js
@@ -1,3 +1,4 @@
+import { styled } from '@mui/material/styles';
 import { memo } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 
@@ -7,12 +8,20 @@ const Leaflet = ({ children, height, ...other }) => {
   return (
     <MapContainer style={{ height }} {...other}>
       <TileLayer
-        attribution='Map tiles by <a href="https://stamen.com/">Stamen Design</a>, under <a href="https://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {children}
     </MapContainer>
   );
 };
 
-export default memo(Leaflet);
+const StyledLeaflet = styled(Leaflet)(({ theme }) => {
+  return {
+    '.leaflet-tile-pane': {
+      filter: 'hue-rotate(40deg) saturate(0.4) contrast(0.6) brightness(1.2)',
+    },
+  };
+});
+
+export default memo(StyledLeaflet);
