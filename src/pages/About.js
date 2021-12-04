a0a3d8f35a357e85eacfecfdf577213274eda275
Stefan Baebler <stefan.baebler@gmail.com>
2021-12-04 01:22:31 +0100

    lint fix

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import enAbout from 'content/en/about.md';
 import slAbout from 'content/sl/about.md';
 import * as Styled from './styles/Markdown';
-import { useRef } from 'react';
 
 const MD = {
   en: enAbout,
