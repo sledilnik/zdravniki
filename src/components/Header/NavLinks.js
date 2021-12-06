340bf03292bcc0166ada3daab40771c75a01cd10
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-12-06 18:17:31 +0100

    FEAT(Header): click on 'Imenik' resets filters

--- src/components/Header/NavLinks.js
+++ src/components/Header/NavLinks.js
@@ -1,14 +1,26 @@
 import { t } from 'i18next';
+import { useNavigate } from 'react-router';
 import i18next from 'i18n';
+import { useFilter } from 'context/filterContext';
 import * as Styled from './styles';
 
 const NavLinks = function NavLinks() {
+  const navigate = useNavigate();
+  const { doctorType, setDoctorType, accept, setAccept, searchValue, setSearchValue } = useFilter();
   const lng = localStorage.getItem('i18nextLng') || 'sl';
   i18next.changeLanguage(lng);
 
+  const goHome = event => {
+    event.preventDefault();
+    if (doctorType !== 'gp') setDoctorType('gp');
+    if (accept !== 'vsi') setAccept('vsi');
+    if (searchValue) setSearchValue('');
+    navigate(`/${lng}/`);
+  };
+
   return (
     <>
-      <Styled.NavMenuLink to={`/${lng}/`} activeclassname="active">
+      <Styled.NavMenuLink to={`/${lng}/`} activeclassname="active" onClick={goHome}>
         {t('header.home')}
       </Styled.NavMenuLink>
       <Styled.NavMenuItemLink
