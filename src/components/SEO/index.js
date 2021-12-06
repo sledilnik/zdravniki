4840a2445444662ea423a9d77ae0e4b970617620
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-12-06 09:41:28 +0100

    REFACTOR(comp...)(SEO): delete Static component

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -1,16 +1,5 @@
 import { Helmet } from 'react-helmet-async';
 
-export const Static = function Static({ children }) {
-  return (
-    <Helmet>
-      <html lang="en" />
-      <title>Hello from React Helmet</title>
-      <meta name="description" content="Basic example" />
-      {children}
-    </Helmet>
-  );
-};
-
 export const Dynamic = function Dynamic({ title, meta = [], lang = 'sl', ...props }) {
   return <Helmet title={title} htmlAttributes={{ lang }} meta={meta} {...props} />;
 };
