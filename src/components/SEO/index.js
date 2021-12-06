fbdcdfe470ec72592594d5c10724a0215b33414c
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-12-06 11:07:13 +0100

    FIX(comp...)(SEO): if all eslint rules are on

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -1,5 +1,18 @@
 import { Helmet } from 'react-helmet-async';
+import PropTypes from 'prop-types';
 
-export const Dynamic = function Dynamic({ title, meta = [], lang = 'sl', ...props }) {
-  return <Helmet title={title} htmlAttributes={{ lang }} meta={meta} {...props} />;
+export const Dynamic = function Dynamic({ title, meta, lang }) {
+  return <Helmet title={title} htmlAttributes={{ lang }} meta={meta} />;
+};
+
+Dynamic.defaultProps = {
+  title: 'Zdravniki - Sledilnik',
+  meta: [],
+  lang: 'sl',
+};
+
+Dynamic.propTypes = {
+  title: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  lang: PropTypes.oneOf('sl', 'en'),
 };
