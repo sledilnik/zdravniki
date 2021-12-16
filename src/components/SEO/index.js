78e8898dcc3cf09ce8ea1382e3cfe9a92460af3d
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-12-16 09:53:15 +0100

    feat(SEO): ✨ add component for JSON - LD

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -1,18 +1,31 @@
 import { Helmet } from 'react-helmet-async';
 import PropTypes from 'prop-types';
 
-export const Dynamic = function Dynamic({ title, meta, lang }) {
-  return <Helmet title={title} htmlAttributes={{ lang }} meta={meta} />;
+export const LdJson = function LdJson({ json }) {
+  return (
+    <Helmet>
+      <script type="application/ld+json">{JSON.stringify(json)}</script>
+    </Helmet>
+  );
+};
+
+LdJson.propTypes = {
+  json: PropTypes.oneOfType([PropTypes.object]).isRequired,
+};
+export const Dynamic = function Dynamic({ title, meta, lang, scripts }) {
+  return <Helmet title={title} htmlAttributes={{ lang }} meta={meta} scripts={scripts} />;
 };
 
 Dynamic.defaultProps = {
   title: 'Zdravniki - Sledilnik',
   meta: [],
-  lang: 'sl',
+  lang: process.env.REACT_APP_DEFAULT_LANGUAGE,
+  scripts: [],
 };
 
 Dynamic.propTypes = {
   title: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
+  meta: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string, content: PropTypes.string })),
   lang: PropTypes.oneOf(['sl', 'en']),
+  scripts: PropTypes.arrayOf(PropTypes.object),
 };
