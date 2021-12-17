24cd2ce11efb58c0d292dd85318a04f7216c9186
Miha Eržen <miha.erzen1@gmail.com>
2021-12-17 21:48:20 +0100

    🍪 Add missing propTypes to Chip

--- src/components/Shared/Chip.js
+++ src/components/Shared/Chip.js
@@ -1,4 +1,5 @@
 import Chip from '@mui/material/Chip';
+import PropTypes from 'prop-types';
 
 const ChipBaseSmall = function ChipBaseSmall(props) {
   return <Chip {...props} size="small" />;
@@ -13,15 +14,30 @@ const ChipSmallOutlined = function ChipSmallOutlined(props) {
 export const Info = function Info({ text, ...props }) {
   return <ChipBaseSmall {...props} label={text} color="info" />;
 };
+Info.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
 export const Success = function Success({ text, ...props }) {
   return <ChipBaseSmall {...props} label={text} color="success" />;
 };
+Success.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
 export const Error = function Error({ text, ...props }) {
   return <ChipBaseSmall {...props} label={text} color="error" />;
+};
+Error.propTypes = {
+  text: PropTypes.string.isRequired,
 };
 
 export const Accepts = function Accepts({ text, accept, ...props }) {
   return accept ? <Success {...props} text={text} /> : <Error {...props} text={text} />;
+};
+Accepts.propTypes = {
+  text: PropTypes.string.isRequired,
+  accept: PropTypes.bool.isRequired,
 };
 
 export const Outlined = ChipSmallOutlined;
@@ -35,6 +51,9 @@ export const OutlinedInfo = function OutlinedInfo({ text, ...props }) {
     />
   );
 };
+OutlinedInfo.propTypes = {
+  text: PropTypes.string.isRequired,
+};
 
 export const FilledError = function FilledError({ text, ...props }) {
   return (
@@ -45,6 +64,10 @@ export const FilledError = function FilledError({ text, ...props }) {
     />
   );
 };
+FilledError.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
 export const FilledSuccess = function FilledSuccess({ text, ...props }) {
   return (
     <ChipSmallFilled
@@ -55,7 +78,14 @@ export const FilledSuccess = function FilledSuccess({ text, ...props }) {
     />
   );
 };
+FilledSuccess.propTypes = {
+  text: PropTypes.string.isRequired,
+};
 
 export const FilledAccepts = function FilledAccepts({ text, accept, ...props }) {
   return accept ? <FilledSuccess {...props} text={text} /> : <FilledError {...props} text={text} />;
+};
+FilledAccepts.propTypes = {
+  text: PropTypes.string.isRequired,
+  accept: PropTypes.bool.isRequired,
 };
