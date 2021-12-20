7c3eb860c2366cfbb1160a0c01dbcd49ceb1d5d5
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-12-20 17:19:27 +0100

    fix: 👽️ use uuid as key for doctors dict
    It's ditry solution but will do for now.
    
    BREAKING CHANGE: #115

--- src/services/doctors.js
+++ src/services/doctors.js
@@ -1,5 +1,4 @@
 import { t } from 'i18next';
-import { v4 as uuidv4 } from 'uuid';
 
 const trimString = str => str.replace(/\s+/g, ' ').trim();
 
@@ -37,12 +36,11 @@ export function createDoctor(doctor, type, institution) {
     post: _post,
   };
 
-  const uuid = uuidv4();
   const { availability, load } = doctor;
 
   return Object.freeze({
     get key() {
-      return uuid;
+      return doctor.key;
     },
     get type() {
       return doctor.type;
@@ -85,12 +83,19 @@ export function createDoctor(doctor, type, institution) {
   });
 }
 
-export default function createDoctors(doctorsDict, institutionsDict, typesDict) {
+export default function createDoctors({
+  doctorsDict,
+  institutionsDict,
+  typesDict,
+  keys = { instKey: 'id_inst', typesKey: 'type' },
+}) {
+  const { instKey, typeKey } = keys;
+
   const doctors = Object.entries(doctorsDict).reduce((acc, [doctorId, doctorData]) => {
     const doctor = createDoctor(
       doctorData,
-      typesDict[doctorData.type],
-      institutionsDict[doctorData.id_inst],
+      typesDict[doctorData[typeKey]],
+      institutionsDict[doctorData[instKey]],
     );
     acc[doctorId] = doctor;
     return acc;
