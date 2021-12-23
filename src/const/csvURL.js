fc9e87b6616a372ea0361068778f4d79f5e8ff60
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-12-23 13:45:18 +0100

    refactor(const): ⚰️ dict-doctors.csv url
    no  longer needed
    
    SEE: 96aa5f43ff63fdcc0cfc053d3c8259e10cdafbcd

--- src/const/csvURL.js
+++ src/const/csvURL.js
@@ -4,9 +4,7 @@ const BASE_URL = new URL(ORIGIN);
 const BASE_PATH = '/sledilnik/zdravniki-data/main/csv';
 
 const INSTITUTIONS_CSV_PATH = `${BASE_PATH}/dict-institutions.csv`;
-const DOCTOR_TYPES_CSV_PATH = `${BASE_PATH}/dict-doctors.csv`;
 const DOCTORS_PATH = `${BASE_PATH}/doctors.csv`;
 
 export const INSTITUTIONS = new URL(INSTITUTIONS_CSV_PATH, BASE_URL);
-export const DOCTOR_TYPES = new URL(DOCTOR_TYPES_CSV_PATH, BASE_URL);
 export const DOCTORS = new URL(DOCTORS_PATH, BASE_URL);
