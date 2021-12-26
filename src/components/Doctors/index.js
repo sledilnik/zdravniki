0a30ad3cb71470d3a957188df3a3b24026dabbe0
bananica <bane@bananica.com>
2021-12-26 19:52:07 +0100

    ADD: Show no results alert when we have no results and there is a search query

--- src/components/Doctors/index.js
+++ src/components/Doctors/index.js
@@ -5,7 +5,9 @@ import { filterContext } from 'context';
 import DoctorCard from 'components/DoctorCard';
 import { useLeafletContext } from 'context/leafletContext';
 import { MAP } from 'const';
+import { t } from 'i18next';
 import L from 'leaflet';
+import { Alert } from '@mui/material';
 import * as Styled from './styles';
 import { MainScrollTop } from '../Shared/ScrollTop';
 import MainMap from './Map';
@@ -53,11 +55,17 @@ const Doctors = function Doctors({ itemsPerPage = 10, useShow }) {
   const center = state?.center ?? MAP.GEO_LOCATION.SL_CENTER;
 
   const areDoctors = Array.isArray(doctors) && doctors.length !== 0;
+  const noResults = !areDoctors && searchValue;
 
   return (
     <Styled.Wrapper show={show}>
       <MainMap className="map" whenCreated={setMap} doctors={doctors} center={center} zoom={zoom} />
       <Styled.WrapperInfinite id="scrollableDiv" className="cards">
+        {noResults && (
+          <Alert sx={{ margin: '24px', borderRadius: '5px' }} severity="info">
+            {t('noResults')}
+          </Alert>
+        )}
         <Styled.InfiniteScroll
           id="infiniteScroll"
           dataLength={doctorsPagination?.length ?? 0}
@@ -73,7 +81,7 @@ const Doctors = function Doctors({ itemsPerPage = 10, useShow }) {
             />
           ))}
         </Styled.InfiniteScroll>
-        {areDoctors && <FooterInfoCard />}
+        <FooterInfoCard />
         <MainScrollTop />
       </Styled.WrapperInfinite>
     </Styled.Wrapper>
