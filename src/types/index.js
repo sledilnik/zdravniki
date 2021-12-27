e4776a46f20b3ba76b41f201841badbafe5a26f9
Miha Eržen <miha.erzen1@gmail.com>
2021-12-27 12:48:26 +0100

    📍 Add prop types to Markers

---
+++ src/types/index.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+export const DoctorPropType = PropTypes.shape({
+  accepts: PropTypes.string,
+  availabilityZZZS: PropTypes.string,
+  availability: PropTypes.string,
+  availabilityOverride: PropTypes.string,
+  email: PropTypes.string,
+  fullAddress: PropTypes.string,
+  geoLocation: PropTypes.shape({
+    lat: PropTypes.number,
+    lon: PropTypes.number,
+  }),
+  instId: PropTypes.string,
+  key: PropTypes.string,
+  load: PropTypes.string,
+  munUnit: PropTypes.string,
+  name: PropTypes.string,
+  nameSlug: PropTypes.string,
+  note: PropTypes.string,
+  orderform: PropTypes.string,
+  phone: PropTypes.string,
+  provider: PropTypes.string,
+  searchAddress: PropTypes.string,
+  type: PropTypes.string,
+  updatedAt: PropTypes.string,
+  website: PropTypes.string,
+  getAcceptText: PropTypes.func,
+  formatUpdatedAt: PropTypes.func,
+});
