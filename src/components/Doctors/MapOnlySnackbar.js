9a9f7df4b16ccf8c918dadd1aea6ef9bf095a7e5
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-12-28 16:39:41 +0100

    feat: ✨ add snackbar alert on mobile in map view

---
+++ src/components/Doctors/MapOnlySnackbar.js
@@ -0,0 +1,60 @@
+import { forwardRef, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { useTranslation } from 'react-i18next';
+import { styled } from '@mui/material/styles';
+
+import MuiSnackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Snackbar = styled(MuiSnackbar)(() => ({
+  '&.MuiSnackbar-anchorOriginTopCenter': {
+    top: '96px',
+  },
+}));
+
+const Alert = forwardRef((props, ref) => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+));
+
+const MapOnlySnackbar = function MapOnlySnackbar({ noResults }) {
+  const { t } = useTranslation();
+  const [open, setOpen] = useState(noResults);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    if (!noResults) {
+      setOpen(false);
+    }
+
+    if (noResults) {
+      setOpen(true);
+    }
+  }, [noResults]);
+
+  return (
+    <Snackbar
+      open={open}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+    >
+      <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+        {t('noResults')}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+MapOnlySnackbar.propTypes = {
+  noResults: PropTypes.bool.isRequired,
+};
+export default MapOnlySnackbar;
