e5890febd1b12e23d876d4511e3334de673fc489
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2022-01-01 10:36:34 +0100

    fix: 🚨 remove unused prop in PageInfo

--- src/components/DoctorCard/PageInfo/index.js
+++ src/components/DoctorCard/PageInfo/index.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import PropTypes from 'prop-types';
 import { CardContent, Typography, Stack, Alert, Tooltip } from '@mui/material';
 import { useNavigate, useLocation, useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -199,12 +198,10 @@ const PageInfo = function PageInfo({ doctor }) {
 
 PageInfo.propTypes = {
   doctor: DoctorPropType,
-  isReportError: PropTypes.func,
 };
 
 PageInfo.defaultProps = {
   doctor: undefined,
-  isReportError: undefined,
 };
 
 export default PageInfo;
