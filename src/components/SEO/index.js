f3016d2ae65d1b07b8a96f5bb77462c6d4c33ad1
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2022-01-18 12:33:51 +0100

    fix(prop-types): 🧑‍💻 it lang missing prop-type

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -1,6 +1,8 @@
 import { Helmet } from 'react-helmet-async';
 import PropTypes from 'prop-types';
 
+import { languages } from 'i18n';
+
 export const Dynamic = function Dynamic({ title, meta, lang }) {
   return <Helmet title={title} htmlAttributes={{ lang }} meta={meta} />;
 };
@@ -14,5 +16,5 @@ Dynamic.defaultProps = {
 Dynamic.propTypes = {
   title: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
-  lang: PropTypes.oneOf(['sl', 'en']),
+  lang: PropTypes.oneOf(languages.map(lang => lang.code)),
 };
