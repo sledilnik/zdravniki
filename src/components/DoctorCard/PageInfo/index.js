3ed1b6e614753bd0286fa79268aff5c56ffeb300
Štefan Baebler <stefan.baebler@gmail.com>
2023-02-08 17:14:37 +0100

    Linter fix again

--- src/components/DoctorCard/PageInfo/index.js
+++ src/components/DoctorCard/PageInfo/index.js
@@ -84,13 +84,14 @@ const PageInfo = function PageInfo({ doctor }) {
           {doctor.name}
         </Typography>
         <Shared.DoubleChip type={type} ageGroup={ageGroup} />
-        <Typography
-          component="h2"
-          variant="h2"
-          className="notranslate">
+        <Typography component="h2" variant="h2" className="notranslate">
           {doctor.provider}
         </Typography>
-        <Typography component="address" variant="body2" className="notranslate" sx={{ mb: { xs: 1, sm: 1.5, md: 2 } }}>
+        <Typography
+          component="address"
+          variant="body2"
+          className="notranslate"
+          sx={{ mb: { xs: 1, sm: 1.5, md: 2 } }}>
           {doctor.fullAddress}
         </Typography>
 
