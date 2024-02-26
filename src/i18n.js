95aa9875ea8ed6623d682829da0e32c50a4c7ca9
Štefan Baebler <stefan.baebler@gmail.com>
2024-02-26 20:47:40 +0100

    i18n: Enabled German translation

--- src/i18n.js
+++ src/i18n.js
@@ -5,12 +5,14 @@ import en from './locales/en.json';
 import sl from './locales/sl.json';
 import it from './locales/it.json';
 import hr from './locales/hr.json';
+import de from './locales/de.json';
 
 export const languages = [
   { name: 'English', code: 'en' },
   { name: 'Slovenščina', code: 'sl' },
   { name: 'Italiano', code: 'it' },
   { name: 'Hrvatski', code: 'hr' },
+  { name: 'Deutsch', code: 'de' },
 ];
 
 i18next
@@ -25,6 +27,7 @@ i18next
       en: { translation: en },
       it: { translation: it },
       hr: { translation: hr },
+      de: { translation: de },
     },
     lng: process.env.REACT_APP_DEFAULT_LANGUAGE, // if you're using a language detector, do not define the lng option
     fallbackLng: languages.map(value => value.code),
