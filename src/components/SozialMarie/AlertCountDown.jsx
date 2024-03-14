5096b15b00c0018ca764ab6e3293bff92b0bd815
Jaka Daneu <jakad@me.com>
2024-03-14 10:02:05 +0100

    feat: ✨ adds variant prop to AlertCountDown component

--- src/components/SozialMarie/AlertCountDown.jsx
+++ src/components/SozialMarie/AlertCountDown.jsx
@@ -1,10 +1,18 @@
+import { SimpleCountDown } from 'components/Shared/CountDown';
 import { t } from 'i18next';
 import PropTypes from 'prop-types';
 import { getTimeDifference } from 'utils';
 
-const AlertCountDown = function AlertCountDown({ date, time }) {
-  const sozialMarieTranslations = t('sozialMarie', { returnObjects: true });
+const AlertCountDown = function AlertCountDown({ date, time, variant = 'simple' }) {
   const { days, hours, minutes, seconds } = getTimeDifference(time);
+  if (variant === 'simple') {
+    return (
+      <SimpleCountDown date={date} days={days} hours={hours} minutes={minutes} seconds={seconds} />
+    );
+  }
+
+  const sozialMarieTranslations = t('sozialMarie', { returnObjects: true });
+
   const daysText = days >= 1 ? `${days} ${t('time.day', { count: days })}, ` : '';
   const hoursText = hours >= 1 ? `${hours} ${t('time.hour', { count: hours })}, ` : '';
   const minutesText =
@@ -22,9 +30,14 @@ const AlertCountDown = function AlertCountDown({ date, time }) {
   );
 };
 
+AlertCountDown.defaultProps = {
+  variant: 'simple',
+};
+
 AlertCountDown.propTypes = {
   date: PropTypes.instanceOf(Date).isRequired,
   time: PropTypes.number.isRequired,
+  variant: PropTypes.oneOf(['simple', 'full']),
 };
 
 export default AlertCountDown;
