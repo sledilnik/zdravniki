559327181afddf2737e727d66bedaf6410ed1bbb
Jaka Daneu <jakad@me.com>
2024-03-16 12:50:26 +0100

    refactor: ♻️ duplicate vars

--- src/components/SozialMarie/date-range.js
+++ src/components/SozialMarie/date-range.js
@@ -1,6 +1,5 @@
+import { ONE_SECOND_MILLISECONDS } from '../../const/time';
 import { getDevVotingDateRange } from './getDevVotingDateRange';
-
-const ONE_DAY = 24 * 60 * 60 * 1000;
 
 // Safari and iOS don't support the date format 'YYYY-MM-DD HH:MM GMT+0200' https://www.coditty.com/code/javascript-new-date-not-working-on-ie-and-safari
 const VOTING_STARTS = 'Tue Apr 09 2024 00:00:00 GMT+0200';
@@ -9,7 +8,7 @@ const VOTING_ENDS = 'Wed Apr 17 2024 00:00:00 GMT+0200';
 const now = new Date(new Date().setMilliseconds(0));
 const dateRange =
   process.env.NODE_ENV === 'development'
-    ? getDevVotingDateRange(now, 5000, ONE_DAY / 24 / 360)
+    ? getDevVotingDateRange(now, 5000, ONE_SECOND_MILLISECONDS * 10)
     : [new Date(VOTING_STARTS), new Date(VOTING_ENDS)];
 
 export const startDate = dateRange[0];
