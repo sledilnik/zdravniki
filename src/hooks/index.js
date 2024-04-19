1b5145ed8fcdb714b12b9335ba8079024789476d
Štefan Baebler <stefan.baebler@gmail.com>
2024-04-19 08:03:15 +0200

    Revert "Feat jalezi/sozial marie banner 466 (#474)"
    This reverts commit 0d008e5637551f8b9a065f93021d6e0f2b5ff768.

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,5 +3,3 @@ export { default as useTimeout } from './useTimeout';
 export { default as useDebounce } from './useDebounce';
 export { default as useGeoLocation } from './useGeoLocation';
 export { default as useEventListener } from './useEventListener';
-export { default as useTimer } from './useTimer';
-export { default as useLocalStorage } from './useLocalStorage';
