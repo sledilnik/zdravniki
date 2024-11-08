fd70f072a820ad7120f4b7ac56efb42daa4a3131
Jaka Daneu <jakad@me.com>
2024-11-08 16:46:06 +0100

    refactor(analytics): 🐛 move fake data and options for RichInfoClick to separate file

--- src/pages/Analytics/RichInfoClick/index.jsx
+++ src/pages/Analytics/RichInfoClick/index.jsx
@@ -6,15 +6,17 @@ import Highcharts from 'highcharts';
 import HighMaps from 'highcharts/highmaps';
 import HighchartsReact from 'highcharts-react-official';
 
-import sloOBMap from 'assets/maps/OB.geo.json';
-
-import sloMunicipalitiesJSON from 'assets/data/slovenia_municipalities.json';
-
 import styles from '../Cards/Card.module.css';
 import stylesRichInfoClick from './RichInfoClick.module.css';
-import { dimensions } from '../HighchartsOptions/options';
 import ChartHeader from '../Cards/ChartHeader';
 import CustomSeriesButtons from '../CustomSeriesButtons';
+import {
+  baseSecondChartOptions,
+  firstChartSeriesMap,
+  mapOptions,
+  secondChartSeriesDataMap,
+  yearsSortedDesc,
+} from './chart-options';
 
 /**
  * @typedef {Object} DataItem
@@ -36,199 +38,6 @@ import CustomSeriesButtons from '../CustomSeriesButtons';
  * @property {string} name
  * @property {SeriesDataItem[]} data
  */
-
-/**
- * @description Transforms the data into a format that Highcharts can understand.
- * @function
- * @param {DataItem[]} data - The data to transform.
- * @returns {SeriesItem[]}
- */
-const transformData = data => {
-  // Initialize an empty object to accumulate the transformed data
-  const result = data.reduce((acc, item) => {
-    // If the accumulator doesn't have an entry for the current item's name, create one
-    if (!acc[item.name]) {
-      acc[item.name] = [];
-    }
-    // Push the current item's year and value as an object into the corresponding name's array
-    acc[item.name].push({ x: item.year, y: item.value });
-    return acc;
-  }, {});
-
-  // Convert the accumulated object into an array of objects with name and data properties
-  return Object.keys(result).map(key => ({
-    id: key,
-    name: key,
-    data: result[key],
-  }));
-};
-
-/**
- * Generates fake data for Slovenian municipalities from 2013 to 2023.
- * @param {string[]} municipalities - An array of Slovenian municipalities.
- * @returns {DataItem[]} An array of objects, each representing data for a municipality in a specific year.
- */
-const generateFakeData = municipalities => {
-  const fakeData = [];
-  for (let i = 2013; i <= 2023; i++) {
-    municipalities.forEach(municipality => {
-      fakeData.push({
-        name: municipality,
-        value: Math.floor(Math.random() * 1000) / 100,
-        OB_UIME: municipality,
-        year: i,
-      });
-    });
-  }
-  return fakeData;
-};
-
-const fakeData = generateFakeData(sloMunicipalitiesJSON);
-
-/**
- * @type {DataItem[]}
- */
-const firstChartSeriesMap = fakeData.reduce((acc, item) => {
-  if (!acc.has(item.year)) {
-    acc.set(item.year, []);
-  }
-  acc.get(item.year).push(item);
-  return acc;
-}, new Map());
-
-const yearsSortedDesc = Array.from(firstChartSeriesMap.keys())
-  .filter(v => !Number.isNaN(v))
-  .sort((a, b) => b - a);
-
-/** @type {Types.HighMapsOptions} */
-const mapOptions = {
-  chart: {
-    map: sloOBMap,
-    spacing: 1,
-  },
-  mapNavigation: {
-    enabled: true,
-    buttonOptions: {
-      verticalAlign: 'bottom',
-      x: 10,
-    },
-  },
-  legend: {
-    enabled: true,
-    align: 'right',
-    verticalAlign: 'bottom',
-    layout: 'vertical',
-    floating: true,
-    useHTML: true,
-    padding: 12,
-  },
-  title: {
-    text: 'Neki po občinah',
-  },
-  subtitle: {
-    text: 'Klikni na občino za dodatne informacije',
-  },
-  tooltip: {
-    footerFormat: '<span style="font-size: 10px">(Click for details)</span>',
-  },
-  colorAxis: {
-    minColor: '#AAE8F8',
-    maxColor: '#095568',
-    startOnTick: true,
-    endOnTick: true,
-    // type: 'logarithmic', can not use logarithmic scale with negative values and 0; use linear scale instead
-  },
-  responsive: {
-    rules: [
-      {
-        condition: {
-          minWidth: dimensions.breakpoints.sm,
-        },
-        chartOptions: {
-          chart: {
-            height: dimensions.height.md,
-          },
-        },
-      },
-      {
-        condition: {
-          maxWidth: dimensions.breakpoints.sm,
-        },
-        chartOptions: {
-          chart: {
-            height: dimensions.height.sm,
-          },
-          legend: {
-            enabled: true,
-            align: 'center',
-            verticalAlign: 'bottom',
-            layout: 'horizontal',
-            floating: false,
-            useHTML: true,
-          },
-        },
-      },
-    ],
-  },
-  series: [
-    {
-      type: 'map',
-      name: 'Slo OB Data',
-      mapData: sloOBMap,
-      joinBy: 'OB_UIME',
-      data: firstChartSeriesMap.get(yearsSortedDesc[0]),
-      allowPointSelect: true,
-      cursor: 'pointer',
-      states: {
-        select: {
-          color: '#95C83F',
-          borderColor: 'black',
-          dashStyle: 'shortdot',
-        },
-      },
-      borderWidth: 0.5,
-      dataLabels: {
-        enabled: true,
-        format: '{point.properties.OB_UIME}',
-        style: {
-          textOutline: 'none',
-        },
-      },
-    },
-  ],
-};
-
-const secondChartSeries = transformData(fakeData);
-const secondChartSeriesDataMap = new Map(secondChartSeries.map(item => [item.name, item]));
-
-/** @type {Types.HighchartsOptions} */
-const baseSecondChartOptions = {
-  chart: {
-    type: 'area',
-  },
-  credits: {
-    enabled: false,
-  },
-  legend: { enabled: false },
-  tooltip: {
-    split: false,
-    shared: true,
-    useHTML: true,
-  },
-  plotOptions: {
-    series: {
-      events: {},
-    },
-  },
-
-  xAxis: {
-    minTickInterval: 1,
-    tickPixelInterval: 50,
-    crosshair: true,
-  },
-
-  series: [],
-};
 
 const RichInfoClick = function RichInfoClick() {
   /** @type {[string, React.Dispatch<React.SetStateAction<Highcharts.Point[]>>]} */
