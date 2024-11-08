4638043d959bb1ae74bd12f32790de39c3133198
Jaka Daneu <jakad@me.com>
2024-11-08 16:16:30 +0100

    fix(analytics): 🐛 viewport does not navigate to the right chart
    When page is loaded and user navigates to the last chart, all charts in viewport are rendered. The height of fake divs (for SEO) don't have enough height, and desired chart is pushed down when all charts are rendered.

--- src/components/RenderOnViewportEntry/index.jsx
+++ src/components/RenderOnViewportEntry/index.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/require-default-props */
-import PropTypes from 'prop-types';
 
 import { Suspense, useRef } from 'react';
 import useFirstViewportEntry from 'hooks/useFirstViewportEntry';
 
 import styles from './RenderOnViewportEntry.module.css';
+
+console.log(styles);
 
 /**
  * Component that renders its children only when it enters the viewport.
@@ -14,6 +16,7 @@ import styles from './RenderOnViewportEntry.module.css';
  * @param {Object} props - The component props.
  * @param {React.ReactNode} props.children - The children to render when the component enters the viewport.
  * @param {IntersectionObserverEntryInit} [props.intersectionObserverInit] - The options for the IntersectionObserver.
+ * @param {React.ComponentProps<'div'>["className"]} [props.divProps] - The props to pass to the wrapping <div></div>
  * @param {React.ComponentProps<'div'>} [props.divProps] - The props to pass to the wrapping <div></div>
  * @returns {JSX.Element} The rendered component.
  */
@@ -21,6 +24,7 @@ const RenderOnViewportEntry = function RenderOnViewportEntry({
   children,
   intersectionObserverInit = { threshold: 0, root: null, rootMargin: '0px' },
   srOnlyComponentsBeforeEntered,
+  className,
   ...divProps
 }) {
   const { threshold = 0, root = null, rootMargin = '0px' } = intersectionObserverInit;
@@ -28,21 +32,11 @@ const RenderOnViewportEntry = function RenderOnViewportEntry({
   const hasEntered = useFirstViewportEntry(ref, { threshold, root, rootMargin });
 
   return (
-    <div ref={ref} className={styles.RenderOnViewportEntry} {...divProps}>
+    <div ref={ref} className={className} {...divProps}>
       {!hasEntered ? srOnlyComponentsBeforeEntered : null}
       {hasEntered ? <Suspense fallback={<div>Loading....</div>}>{children}</Suspense> : null}
     </div>
   );
-};
-
-RenderOnViewportEntry.propTypes = {
-  children: PropTypes.node.isRequired,
-  intersectionObserverInit: PropTypes.shape({
-    threshold: PropTypes.number,
-    root: PropTypes.instanceOf(Element),
-    rootMargin: PropTypes.string,
-  }),
-  srOnlyComponentsBeforeEntered: PropTypes.node,
 };
 
 export default RenderOnViewportEntry;
