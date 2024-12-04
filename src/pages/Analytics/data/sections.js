41866787d25e9e760ed3f2d249e521714dfd56c1
Jaka Daneu <jakad@me.com>
2024-12-04 06:29:45 +0100

    refactor(Analytics): update fakeHeight values and add TODO for height adjustments

--- src/pages/Analytics/data/sections.js
+++ src/pages/Analytics/data/sections.js
@@ -1,5 +1,13 @@
 /** @import * as Types from "../types" */
 
+/**
+ * TODO
+ *  - check for actual fake height values; card height can be different on mobile or desktop or other devices:
+ *  fakeHeight is needed to simulate the height of the card in the editor so the scrolling is correct if card is not rendered yet
+ *  and user navigates to it via the sidebar
+ *
+ */
+
 import { mapOptions as RichInfoClickOptions } from '../Cards/RichInfoClick/chart-options';
 import { mapOptions as RomanLustrikCardMapOptions } from '../Cards/DataByYearAndAgeGroupCard/char-options';
 import { availabilityChangeByInstitutionType } from './production/availability-change-by-institution-type';
@@ -9,7 +17,7 @@ export const charts = [
     section: 'real section 1',
     order: 5,
     componentName: 'MotionCard',
-    fakeHeight: '2876px', // todo set height when dashboard is done
+    fakeHeight: '2876px',
     options: {
       title: {
         text: 'MotionCard',
@@ -20,7 +28,7 @@ export const charts = [
     section: 'real section 1',
     order: 0,
     componentName: 'DashboardCard',
-    fakeHeight: '683px', // todo set height when dashboard is done
+    fakeHeight: '683px',
     options: {
       title: {
         text: 'Dashboard',
