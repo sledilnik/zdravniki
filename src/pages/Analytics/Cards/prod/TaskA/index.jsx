363969e8cf42b3093f20f4645f28b1923da15c20
Jaka Daneu <jakad@me.com>
2025-01-06 17:44:12 +0100

    feat(Analytics): extract useCharts hook for better code organization and reusability

--- src/pages/Analytics/Cards/prod/TaskA/index.jsx
+++ src/pages/Analytics/Cards/prod/TaskA/index.jsx
@@ -18,76 +18,13 @@ import { mapOptions, secondChartOptions } from './chart-options';
 import styles from '../MapAndChart.module.css';
 import {
   DEFAULTS,
-  prepareDetailLineChartSeries,
   prepareOverviewMapSeriesData,
   uniqueDoctorTypesSet,
   uniqueMunicipalitiesSet,
   uniqueYearsSet,
 } from './data';
 import { FilterForm } from './FilterForm';
-
-/**
- *
- * @param {Types.UserInputsValues} initialFilterState
- * @param {{map: HighMaps.Options, chart: Highcharts.Options}} options
- * @param {boolean} init
- * @returns
- */
-const useCharts = (initialFilterState, options, init) => {
-  const [filterState, setFilterState] = useState(initialFilterState);
-  const [mapChartOptions, setMapChartOptions] = useState(options.map);
-  const [chartOptions, setChartOptions] = useState(options.chart);
-  /** @type {[Types.OverviewDataMapSeriesDataItem, React.Dispatch<React.SetStateAction<Types.OverviewDataMapSeriesDataItem>>]} */
-  const [selectedPoint, setSelectedPoint] = useState(
-    options.map.series[0].data.find(d => d.selected),
-  );
-
-  useEffect(() => {
-    if (!init) return;
-    setMapChartOptions({
-      plotOptions: {
-        series: {
-          point: {
-            events: {
-              click(e) {
-                e.point.select(true, false);
-                setFilterState(prev => ({
-                  ...prev,
-                  municipality: e.point.name,
-                }));
-              },
-            },
-          },
-        },
-      },
-    });
-  }, [init]);
-
-  useEffect(() => {
-    const data = prepareOverviewMapSeriesData(filterState);
-    setMapChartOptions({
-      series: [{ data }],
-    });
-
-    const sPoint = data.find(d => d.selected);
-    setSelectedPoint(sPoint);
-
-    const series = prepareDetailLineChartSeries(filterState);
-    setChartOptions({
-      series,
-    });
-  }, [filterState]);
-
-  return {
-    filterState,
-    mapChartOptions,
-    setFilterState,
-    setMapChartOptions,
-    chartOptions,
-    setChartOptions,
-    selectedPoint,
-  };
-};
+import { useCharts } from './hooks';
 
 /**
  * TaskA component
